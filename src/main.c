7f5dc238a0dc92722e37f17f7a56c4ca75f5bd63
connie <181677440+rvl-005@users.noreply.github.com>
2025-06-21 10:37:50 -0400

    forgot to add this to github (sorry)

---
+++ src/main.c
@@ -0,0 +1,32 @@
+// gcc src/main.c -o bin/main.exe
+#include <stdio.h>
+#include <string.h>
+// This program prompts the user to input a string, and then outputs a file with said string.
+
+FILE *fptr;
+char inputString[];
+
+void addString() {
+    // Prompts user for input
+    printf("Enter your first name: \n");
+    // processes input
+    scanf("%s", inputString);
+}
+void createFile() {
+    // Opens file for writing
+    fptr = fopen("output.txt", "w");
+    if (fptr == NULL) {
+        printf("Error opening file!\n");
+        return;
+    }
+    // Writes input string to file
+    fprintf(fptr, "%s\n", inputString);
+    // Closes the file
+    fclose(fptr);
+}
+int main() {
+    addString();
+    createFile();
+    printf("String written to file successfully.\n");
+    return 0;
+}
